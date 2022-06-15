fe3c75cda6aa39fce56555768e7967b1c40c7b4f
cdargentre <cdargentre@gmail.com>
2022-06-15 16:35:13 +0200

    csrfProtection mis sur route post

--- Routes/meetingRoute.js
+++ Routes/meetingRoute.js
@@ -3,7 +3,10 @@ const router = express.Router()
 
 const MeetingController = require('../Controllers/meetingController')
 
+const csurf = require('csurf')
+const csrfProtection = csurf({ cookie: true });
+
 router.get('/', MeetingController.findAllMeetings)
-router.post('/', MeetingController.createMeeting)
+router.post('/', csrfProtection, MeetingController.createMeeting)
 
 module.exports = router
